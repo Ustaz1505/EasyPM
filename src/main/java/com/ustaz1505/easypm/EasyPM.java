41fd7239091cd581c466e10c5fe4556ff14681b2
Ustaz1505 <nikma-1@Outlook.com>
2023-04-10 22:55:36 +0300

    Base commit

---
+++ src/main/java/com/ustaz1505/easypm/EasyPM.java
@@ -0,0 +1,17 @@
+package com.ustaz1505.easypm;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EasyPM extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
