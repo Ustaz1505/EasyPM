1327c2d0b58a170b13211b49c294c0641ee130df
Ustaz1505 <nikma-1@Outlook.com>
2023-04-11 12:19:15 +0300

    Version 1.1 Release: PM command finished!

--- src/main/java/com/ustaz1505/easypm/EasyPM.java
+++ src/main/java/com/ustaz1505/easypm/EasyPM.java
@@ -3,15 +3,18 @@ package com.ustaz1505.easypm;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
+import java.util.logging.Logger;
 
 public final class EasyPM extends JavaPlugin {
-
-    String logPrefix = this.getConfig().getString("log-prefix");
+    FileConfiguration config = this.getConfig();
+    String logPrefix = config.getString("log-prefix");
+    Logger logger = this.getLogger();
 
     @Override
     public void onEnable() {
@@ -30,30 +33,35 @@ public final class EasyPM extends JavaPlugin {
 
         // This method is called, when somebody uses our command
         @Override
-        public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
-                if (args.length < 2) {
-                    player.sendMessage("Используй /pm <Ник> <Сообщение>!");
-                    return true;
-                }
-                Player target = getServer().getPlayer(args[0]);
-                if (target == null) {
-                    player.sendMessage("Игрок не в сети!");
-                    return true;
-                }
-
-                StringBuilder message = new StringBuilder();
-
-                for(int i = 1; i < args.length - 1; i++){ //loop threw all the arguments
-                    String arg = args[i] + " "; //get the argument, and add a space so that the words get spaced out
-                    message.append(arg); //add the argument to myString
-                }
-                message.append(args[args.length - 1]);
-
-                target.sendMessage("Игрок " + player.getName() + " отправил сообщение \"" + message + "\"");
+        public boolean onCommand(@NotNull CommandSender sender, @ NotNull Command command, @NotNull String label, String[] args) {
+            if (!(sender instanceof Player)) {
+                logger.info(logPrefix + " Команда может быть выполнена только пользователем!");
+                return true;
             }
-            // If the player (or console) uses our command correct, we can return true
+            Player player = (Player) sender;
+            if (args.length < 2) {
+                player.sendMessage("Используй /pm <Ник> <Сообщение>!");
+                return true;
+            }
+            Player target = getServer().getPlayer(args[0]);
+            if (target == null) {
+                player.sendMessage("Игрок не в сети!");
+                return true;
+            }
+
+            StringBuilder message = new StringBuilder();
+
+            for(int i = 1; i < args.length - 1; i++){ //loop threw all the arguments
+                String arg = args[i] + " "; //get the argument, and add a space so that the words get spaced out
+                message.append(arg); //add the argument to myString
+            }
+            message.append(args[args.length - 1]);
+
+            String senderMsgFormat = config.getString("pm-sender-message");
+            String receiverMsgFormat = config.getString("pm-receiver-message");
+
+            target.sendMessage(receiverMsgFormat.replace("{receiver-name}", target.getName()).replace("{sender-name}", player.getName()).replace("{message}", message));
+            player.sendMessage(senderMsgFormat.replace("{receiver-name}", target.getName()).replace("{sender-name}", player.getName()).replace("{message}", message));
             return true;
         }
     }
